439d2725a21222a7c4e31083905876f28146bca7
ronny macmaster <ronnymacmaster@gmail.com>
2016-03-27 12:03:56 -0500

    gui

--- src/gui/PLogGUI.java
+++ src/gui/PLogGUI.java
@@ -7,9 +7,12 @@
  ***************************************************************/
 package gui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
 import jpcap.*;
 import java.io.*;
 
@@ -23,6 +26,7 @@ public class PLogGUI{
 	boolean CaptureState = false;
 
 	/** GUI Globals */
+	BufferedImage guiIcon;
 	JFrame mainWindow = new JFrame("Habbo Packet_Logger v1.0 - ©2016 Ronny Macmaster");
 	JTextArea TA_OUTPUT = new JTextArea();
 	JScrollPane SP_OUTPUT = new JScrollPane();
@@ -72,8 +76,14 @@ public class PLogGUI{
 
 	/** GUI Constructor */
 	public PLogGUI(){
-		buildGUI();
-		// disableButtons();
+		try{
+			guiIcon = ImageIO.read(new File("icon.bmp"));
+			buildGUI();
+			// disableButtons();
+		} catch(IOException e){
+			System.err.println("Failled to build GUI! exiting...");
+		}
+		
 	}
 
 	/** BuildGUI() initializes the PLogGUI settings */
@@ -88,6 +98,7 @@ public class PLogGUI{
 		// using absolute layout
 		mainWindow.setSize(width, height);
 		mainWindow.setLocation((s_width - width) / 2, (s_height - height) / 2);
+		mainWindow.setIconImage(guiIcon);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.getContentPane().setLayout(null);
 
@@ -111,7 +122,7 @@ public class PLogGUI{
 		 **************************************************************************************************/
 
 		// CAPTURE Button
-		B_CAPTURE.setBackground(Color.RED);
+		B_CAPTURE.setBackground(Color.green);
 		B_CAPTURE.setForeground(Color.white);
 		B_CAPTURE.setBounds((width / 60), (height * 5 / 8), (width * 1 / 8), (height * 1 / 24));
 		B_CAPTURE.addActionListener(new ActionListener(){
@@ -155,15 +166,15 @@ public class PLogGUI{
 		mainWindow.getContentPane().add(B_LIST);
 
 		// FILTER Button
-		B_CAPTURE.setBackground(Color.green);
-		B_CAPTURE.setForeground(Color.white);
-		B_CAPTURE.setBounds((width / 60), (height * 5 / 8), (width * 1 / 8), (height * 1 / 24));
-		B_CAPTURE.addActionListener(new ActionListener(){
+		B_FILTER.setBackground(Color.black);
+		B_FILTER.setForeground(Color.white);
+		B_FILTER.setBounds((width * 31 / 60), (height * 27 / 32), (width * 1 / 8), (height * 1 / 24));
+		B_FILTER.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent X){
-				Action_B_CAPTURE(X);
+				Action_B_FILTER(X);
 			}
 		});
-		mainWindow.getContentPane().add(B_CAPTURE);
+		mainWindow.getContentPane().add(B_FILTER);
 
 		// INFO Button
 		B_INFO.setBackground(Color.black);
@@ -225,26 +236,10 @@ public class PLogGUI{
 		 * 
 		 * Control Network Filtering (unique selection)
 		 **************************************************************************************************/
-		JRadioButton RB_Port_Special = new JRadioButton("Special Port");
-		JRadioButton RB_Port_HTTP = new JRadioButton("HTTP (80)");
-		JRadioButton RB_Port_SSL = new JRadioButton("SSL (443)");
-		JRadioButton RB_Port_FTP = new JRadioButton("FTP (21)");
-		JRadioButton RB_Port_SSH = new JRadioButton("SSH (22)");
-		JRadioButton RB_Port_TelNet = new JRadioButton("TelNet (23)");
-		JRadioButton RB_Port_SMTP = new JRadioButton("SMTP (25)");
-		JRadioButton RB_Port_POP3 = new JRadioButton("POP3 (110)");
-		JRadioButton RB_Port_IMAP = new JRadioButton("IMAP (143)");
-		JRadioButton RB_Port_IMAPS = new JRadioButton("IMAPS (993)");
-		JRadioButton RB_Port_DNS = new JRadioButton("DNS (53)");
-		JRadioButton RB_Port_NetBIOS = new JRadioButton("NetBIOS (139)");
-		JRadioButton RB_Port_SAMBA = new JRadioButton("SAMBA (137)");
-		JRadioButton RB_Port_AD = new JRadioButton("AD (445)");
-		JRadioButton RB_Port_SQL = new JRadioButton("SQL (118)");
-		JRadioButton RB_Port_LDAP = new JRadioButton("LDAP (389)");
 
 		// ENABLE filter
 		BG_Filters.add(RB_FilterEnable);
-		RB_FilterEnable.setBounds((width * 26 / 60), (height * 11 / 16), (width * 1 / 12),
+		RB_FilterEnable.setBounds((width * 26 / 60), (height * 12 / 16), (width * 1 / 12),
 				(height * 1 / 24));
 		RB_FilterEnable.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent X){
@@ -256,7 +251,7 @@ public class PLogGUI{
 		// DISABLE filter
 		BG_Filters.add(RB_FilterDisable);
 		RB_FilterDisable.setSelected(true);
-		RB_FilterDisable.setBounds((width * 31 / 60), (height * 11 / 16), (width * 1 / 10),
+		RB_FilterDisable.setBounds((width * 31 / 60), (height * 12 / 16), (width * 1 / 10),
 				(height * 1 / 24));
 		RB_FilterDisable.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent X){
@@ -265,11 +260,118 @@ public class PLogGUI{
 		});
 		mainWindow.getContentPane().add(RB_FilterDisable);
 		
+		//Special
+		BG_Ports.add(RB_Port_Special);
+		RB_Port_Special.setBounds((width * 31 / 60), (height * 51 / 64), (width * 1 / 8),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_Special);
+		
 		//HTTP
 		BG_Ports.add(RB_Port_HTTP);
-		RB_Port_HTTP.setBounds((width * 38 / 60), (height * 11 / 16), (width * 1 / 8),
+		RB_Port_HTTP.setFont(new Font("Serif", 0, 9));
+		RB_Port_HTTP.setBounds((width * 39 / 60), (height * 42 / 64), (width * 1 / 10),
 				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_HTTP);
+		
+		//SSL
+		BG_Ports.add(RB_Port_SSL);
+		RB_Port_SSL.setFont(new Font("Serif", 0, 9));
+		RB_Port_SSL.setBounds((width * 39 / 60), (height * 45 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_SSL);
+		
+		//FTP
+		BG_Ports.add(RB_Port_FTP);
+		RB_Port_FTP.setFont(new Font("Serif", 0, 9));
+		RB_Port_FTP.setBounds((width * 39 / 60), (height * 48 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_FTP);
+		
+		//SSH
+		BG_Ports.add(RB_Port_SSH);
+		RB_Port_SSH.setFont(new Font("Serif", 0, 9));
+		RB_Port_SSH.setBounds((width * 39 / 60), (height * 51 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_SSH);
+		
+		//TelNet
+		BG_Ports.add(RB_Port_TelNet);
+		RB_Port_TelNet.setFont(new Font("Serif", 0, 9));
+		RB_Port_TelNet.setBounds((width * 39 / 60), (height * 54 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_TelNet);
+		
+		//SMTP
+		BG_Ports.add(RB_Port_SMTP);
+		RB_Port_SMTP.setFont(new Font("Serif", 0, 9));
+		RB_Port_SMTP.setBounds((width * 45 / 60), (height * 42 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_SMTP);
 
+		//POP3
+		BG_Ports.add(RB_Port_POP3);
+		RB_Port_POP3.setFont(new Font("Serif", 0, 9));
+		RB_Port_POP3.setBounds((width * 45 / 60), (height * 45 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_POP3);
+
+		//IMAP
+		BG_Ports.add(RB_Port_IMAP);
+		RB_Port_IMAP.setFont(new Font("Serif", 0, 9));
+		RB_Port_IMAP.setBounds((width * 45 / 60), (height * 48 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_IMAP);
+
+		//IMAPS
+		BG_Ports.add(RB_Port_IMAPS);
+		RB_Port_IMAPS.setFont(new Font("Serif", 0, 9));
+		RB_Port_IMAPS.setBounds((width * 45 / 60), (height * 51 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_IMAPS);
+
+		//DNS
+		BG_Ports.add(RB_Port_DNS);
+		RB_Port_DNS.setFont(new Font("Serif", 0, 9));
+		RB_Port_DNS.setBounds((width * 45 / 60), (height * 54 / 64), (width * 1 / 10),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_DNS);
+
+		//NetBIOS
+		BG_Ports.add(RB_Port_NetBIOS);
+		RB_Port_NetBIOS.setFont(new Font("Serif", 0, 9));
+		RB_Port_NetBIOS.setBounds((width * 51 / 60), (height * 42 / 64), (width * 1 / 9),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_NetBIOS);
+		
+		//SAMBA
+		BG_Ports.add(RB_Port_SAMBA);
+		RB_Port_SAMBA.setFont(new Font("Serif", 0, 9));
+		RB_Port_SAMBA.setBounds((width * 51 / 60), (height * 45 / 64), (width * 1 / 9),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_SAMBA);
+
+		//AD
+		BG_Ports.add(RB_Port_AD);
+		RB_Port_AD.setFont(new Font("Serif", 0, 9));
+		RB_Port_AD.setBounds((width * 51 / 60), (height * 48 / 64), (width * 1 / 9),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_AD);
+		
+		//SQL
+		BG_Ports.add(RB_Port_SQL);
+		RB_Port_SQL.setFont(new Font("Serif", 0, 9));
+		RB_Port_SQL.setBounds((width * 51 / 60), (height * 51 / 64), (width * 1 / 9),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_SQL);
+		
+		//LDAP
+		BG_Ports.add(RB_Port_LDAP);
+		RB_Port_LDAP.setFont(new Font("Serif", 0, 9));
+		RB_Port_LDAP.setBounds((width * 51 / 60), (height * 54 / 64), (width * 1 / 9),
+				(height * 1 / 24));
+		mainWindow.getContentPane().add(RB_Port_LDAP);
+		
+		
 		mainWindow.setVisible(true);
 	}
 
